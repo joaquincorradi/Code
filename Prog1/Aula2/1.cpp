ec6427c058bed88f0dc0cc784827033f808f86d8
corradijoaquin(elementaryOS) <corradijoaquin@gmail.com>
2022-08-23 00:44:37 -0300

    Agregando Aula2

---
+++ Prog1/Aula2/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+float calcPerimetro(float base, float altura)
+{
+  return 2. * (base + altura);
+}
+
+float calcArea(float base, float altura)
+{
+  return base * altura;
+}
+
+int main()
+{
+  float base, altura;
+
+  while (base < 0 || altura < 0)
+  {
+    std::cin >> base >> altura;
+  }
+  std::cout << calcPerimetro(base, altura) << std::endl;
+  std::cout << calcArea(base, altura) << std::endl;
+}
---
+++ Prog1/Aula2/2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+float perimetroCircunferecia(float radio)
+{
+  return 2 * M_PI * radio;
+}
+
+int main()
+{
+  float radio;
+  
+}
