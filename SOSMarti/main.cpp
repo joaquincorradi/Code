484a0aaf6d7cf1141e9dff8c1f769ffd2529459b
corradijoaquin(elementaryOS) <corradijoaquin@gmail.com>
2023-02-27 12:16:59 -0300

    Trabajando en final

--- SOSMarti/main.cpp
+++ SOSMarti/main.cpp
@@ -20,25 +20,26 @@ void guardarDatos() {
   std::ofstream datosCensado("datos.txt");
   int temp1, temp3, temp4;
   char temp2;
-  for (int i = 0; i < 3; ++i) {
+  for (int i = 0; i < 10; ++i) {
     std::cout << "Ingrese a continuacion el dato de la persona nro. " << i + 1
               << '\n';
     std::cout << "Edad: ";
     std::cin >> temp1;
     datosCensado << temp1 << '\t';
+
     std::cout << "Genero [M-F]: ";
     std::cin >> temp2;
     datosCensado << temp2 << '\t';
+
     std::cout << "Ingresos: ";
     std::cin >> temp3;
     datosCensado << temp3 << "\t";
+
     std::cout << "Region [1-3]: ";
     std::cin >> temp4;
     datosCensado << temp4 << '\n';
   }
+  datosCensado.close();
 }
 
-void leerDatos(persona datos[10]) {
-  // ...
-  // ...
-}
+void leerDatos(persona datos[10]) {}
