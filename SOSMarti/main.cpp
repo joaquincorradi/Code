acb17490f2e50436775af29ac003b292abbbdde7
corradijoaquin(elementaryOS) <corradijoaquin@gmail.com>
2023-02-27 12:07:20 -0300

    Trabajando en final

--- SOSMarti/main.cpp
+++ SOSMarti/main.cpp
@@ -3,7 +3,7 @@
 
 struct persona {
   int edad;
-  char sexo;
+  char genero;
   int ingresos;
   int region;
 };
@@ -12,7 +12,7 @@ void menu();
 void guardarDatos(persona datos[10]);
 void leerDatos();
 int mediaIngresos();
-void sexoPorRegion();
+void generoPorRegion();
 
 int main() {}
 
@@ -22,13 +22,26 @@ void menu() {
 
 void guardarDatos(persona datos[10]) {
   std::ofstream datosCensado("datos.txt");
-  int temp1, temp2, temp4;
-  char temp3;
+  int temp1, temp3, temp4;
+  char temp2;
   for (int i = 0; i < 10; ++i) {
     std::cout << "Ingrese a continuacion el dato de la persona nro. " << i + 1
               << '\n';
     std::cout << "Edad: ";
     std::cin >> temp1;
     datosCensado << temp1;
+    std::cout << "Genero [M-F]: ";
+    std::cin >> temp2;
+    datosCensado << temp2;
+    std::cout << "Ingresos: ";
+    std::cin >> temp3;
+    datosCensado << temp3;
+    std::cout << "Region [1-3]: ";
+    std::cin >> temp4;
+    datosCensado << temp4 << '\n';
   }
+}
+
+void leerDatos(persona datos[10]) {
+  // ...
 }
